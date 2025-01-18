9da6060710d6f3f0138523234ad527372957f2ba
sujit12x10 <sujit12x10@gmail.com>
2025-01-18 18:39:18 +0530

    config index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
 const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+
+// Middleware
+app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => res.send('Home Page Route'));
 
@@ -10,10 +16,41 @@ app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
 app.get('/contact', (req, res) => res.send('Contact Page Route'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 7000;
 
-app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+// here
 
-// "scripts": {
-  //   "test": "echo \"Error: no test specified\" && exit 1"
-// },
+// Checkout API
+app.get('/', (req, res) => res.send('Home Page Route'));
+app.post("/api/create-checkout-session", async(req, res) => {
+    const {products} = req.body
+
+    const lineItems = products.map((product) => ({
+        price_data: {
+            currency: "inr",
+            product_data: {
+                name:product.name,
+                images: [product.image_url1]
+            },
+            unit_amount: product.price * 100
+        },
+        quantity: product.quantity
+    }))
+
+    const couponCode = req.body.coupon ? [{coupon: req.body.coupon}] : []
+    
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: lineItems,
+        mode: "payment",
+        success_url: "/api/success?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url: "/api/cancel",  
+        discounts: couponCode, 
+    })
+
+    res.json({id: session.id})
+})
+
+app.listen(port, () => {
+    console.log("server started")
+})
